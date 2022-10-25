9bcdb1c2e44f2be23ac0193f8e0219a2a9e1e711
vsilagy <vladsilagy@gmail.com>
2022-10-24 23:58:39 -0400

    add github users API, add props to results card

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
+import { useEffect, useState } from 'react';
 import Nav from './components/Nav';
 import SearchInput from './components/SearchInput';
 import ResultsCard from './components/ResultsCard';
 
 export default function App() {
+	let API = 'https://api.github.com/users/octocat';
+	const [text, setText] = useState('');
+	const [user, setUser] = useState('');
+	const [data, setData] = useState('');
+
+	useEffect(() => {
+		if (user) {
+			API = `https://api.github.com/users/${user}`;
+		}
+		fetchUser();
+	}, [user]);
+
+	const fetchUser = async () => {
+		const response = await fetch(API);
+		const data = await response.json();
+		setData(data);
+	};
+
 	return (
 		<div className="max-w-screen max-h-screen mx-auto px-4">
 			<Nav />
 			<SearchInput />
-			<ResultsCard />
+			<ResultsCard data={data} />
 		</div>
 	);
 }
--- src/components/ResultsCard.js
+++ src/components/ResultsCard.js
@@ -2,50 +2,57 @@ import location from '../assets/icon-location.svg';
 import twitter from '../assets/icon-twitter.svg';
 import website from '../assets/icon-website.svg';
 import company from '../assets/icon-company.svg';
-export default function ResultsCard() {
+export default function ResultsCard(props) {
 	return (
-		<section className="max-w-xl mx-auto p-8 rounded-xl shadow-lg bg-white dark:bg-darkSaturatedBlue md:max-w-3xl">
-			<div className="flex justify-start gap-8 mb-8 p-4">
-				<h6 className="h-24 w-24  bg-red-500 rounded-full md:h-36 md:w-36"></h6>
+		<section className="max-w-xl mx-auto p-4 rounded-xl shadow-lg bg-white dark:bg-darkSaturatedBlue md:p-8 md:max-w-3xl">
+			<div className="flex justify-start gap-8 mb-8 p-2 md:p-4">
+				<img
+					src={props.data.avatar_url}
+					className="h-24 w-24 rounded-full md:h-36 md:w-36"
+				/>
 				<div className="flex flex-col gap-4  md:flex-row md:justify-between md:flex-1">
 					<div>
 						<h2 className="text-2xl  font-bold mb-1 md:text-3xl">
-							name
+							{props.data.name}
 						</h2>
 						<h3 className="text-sm text-blueish md:-text-base">
-							@username
+							@{props.data.login}
 						</h3>
 					</div>
-					<p className="text-sm md:text-base">joined</p>
+					<p className="text-sm md:text-base ">
+						{props.data.created_at.slice(0, 10)}
+					</p>
 				</div>
 			</div>
 			<div className="ml-0 md:ml-44">
-				<p className="mx-4 my-6 md:-mt-24">bio</p>
-				<div className="grid grid-cols-3 gap-2 mx-4 mb-4 p-4 text-sm rounded-xl bg-saturatedWhite  dark:bg-veryDarkBlue md:text-base md:gap-4 md:mb-8 md:p-8">
+				<p className="mx-2 my-6 dark:text-white md:mx-4 md:-mt-24">
+					{props.data.bio}
+				</p>
+				<div className="grid grid-cols-3 gap-2 mx-2 my-4 px-4 py-4 text-sm rounded-xl bg-saturatedWhite  dark:bg-veryDarkBlue md:text-base md:gap-4 md:mx-4 md:my-8 md:px-8">
 					<p>Repos</p>
 					<p>Followers</p>
 					<p>Following</p>
-					<p className="font-bold">1</p>
-					<p className="font-bold">2</p>
-					<p className="font-bold">3</p>
+					<p className="font-bold">{props.data.public_repos}</p>
+					<p className="font-bold">{props.data.followers}</p>
+					<p className="font-bold">{props.data.following}</p>
 				</div>
 
-				<div className="grid grid-cols-1 gap-3 mx-4 text-sm md:grid-cols-2 md:gap-5 md:text-base">
+				<div className="grid grid-cols-1 gap-4 mx-2 text-sm md:grid-cols-2 md:gap-6 md:mx-4 md:text-base">
 					<div className="flex gap-4 items-center">
-						<img src={location} className="w-5 md:w-6" />
-						<p>Location</p>
+						<img src={location} className="w-5 md:h-6" />
+						<p>{props.data.location}</p>
 					</div>
 					<div className="flex gap-4 items-center">
 						<img src={twitter} className="w-5 md:w-6" />
-						<p>Twitter</p>
+						<p>{props.data.twitter_username}</p>
 					</div>
 					<div className="flex gap-4  items-center">
 						<img src={website} className="w-5 md:w-6" />
-						<p>Website</p>
+						<p>{props.data.blog}</p>
 					</div>
 					<div className="flex gap-4  items-center">
 						<img src={company} className="w-5 md:w-6" />
-						<p>Company</p>
+						<p>{props.data.company}</p>
 					</div>
 				</div>
 			</div>
